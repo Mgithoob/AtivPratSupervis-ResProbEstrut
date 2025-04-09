d942efa701fcac7dd5292ed2391ea37a910bae9a
Mgithoob <132958332+Mgithoob@users.noreply.github.com>
2025-04-09 04:29:37 -0300

    Update Pilha_l.java

--- AtivPratSupervis/src/Pilha_l.java
+++ AtivPratSupervis/src/Pilha_l.java
@@ -6,14 +6,21 @@ public class Pilha_l {
 
     public Pilha_l()
     {
-        ListaEncadeada lista = new ListaEncadeada();
+        this.dados = new ListaEncadeada();
         // Usando 0 para representar vazio.
-        lista.inserir(0);
-        topo = lista.inicio;
+        this.dados.inserir(0);
+        topo = this.dados.inicio;
     }
 
     public int olharTopo() {
-        if(!vazia()) return topo.dado;
+        if(!vazia()) {
+            ListaEncadeada.No procura = this.dados.inicio;
+            // procurar pos fim
+            while (procura.proximo != topo) {
+                procura = procura.proximo;
+            }
+            return procura.dado;
+        }
 
         System.out.println("A pilha está vazia. Não é possível acessar elementos.");
         return -1;
@@ -21,13 +28,8 @@ public class Pilha_l {
 
     // Adicionar ao topo da pilha
     public void adicionar(int novo_item) {
-        // caso inicial
-        if (vazia()){
-            topo.dado = novo_item;
-            return;
-        }
-
-        dados.inserir(novo_item);
+        topo.dado = novo_item;
+        this.dados.inserir(0);
         topo = topo.proximo;
     }
 
@@ -36,8 +38,10 @@ public class Pilha_l {
     public int retirar() {
         if (!vazia()){
             int elemento = topo.dado;
-            dados.remover(0);
-            topo = dados.inicio;
+            System.out.printf("Retirando o elemento [%d]...", elemento);
+
+            this.dados.remover(0);
+            topo = this.dados.inicio;
             return elemento;
         }
 
@@ -47,13 +51,20 @@ public class Pilha_l {
 
     // Verifica se pilha está vazia.
     private boolean vazia(){
-        if (topo.dado == 0) return true;
-        else return false;
+        ListaEncadeada.No procura = this.dados.inicio;
+
+        // procurar pos final
+        while (procura.proximo != topo) {
+            if (procura.dado != 0) return false;
+            procura = procura.proximo;
+        }
+
+        return true;
     }
 
     public void imprimir(boolean debug)
     {
-        dados.imprimir();
-        if (debug) System.out.printf("Dado topo: %d\n\n", topo.dado);
+        this.dados.imprimir();
+        if (debug && (topo != null)) System.out.printf("Dado topo: %d\n\n", topo.dado);
     }
 }
